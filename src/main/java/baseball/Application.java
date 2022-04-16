73eba6c6ac2959df479bff7247dc734c83324da6
bestsilver <dkvkxm99@gmail.com>
2022-04-16 16:43:39 +0900

    feat: [Add] main 에서 로직 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameState opinion = GameState.restart;
+
+        while (opinion == GameState.restart) {
+            GameRunner runner = new GameRunner();
+            runner.run();
+            opinion = new OpinionInput().getOpinition();
+        }
+
     }
+
 }
