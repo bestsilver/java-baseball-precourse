9bf1da6a359a2cd15e786023b6b4487bdc273814
bestsilver <dkvkxm99@gmail.com>
2022-04-18 01:29:10 +0900

    refactor: GameRule enum으로 사용하지 않아도 될 것 같아서 변경

--- src/main/java/baseball/GameRule.java
+++ src/main/java/baseball/GameRule.java
@@ -1,12 +1,7 @@
 package baseball;
 
-public enum GameRule {
-    MaxLength(3),
-    MaxValue(9),
-    MinValue(1);
+public class GameRule {
     public static final int MAX_LENGTH = 3;
     public static final int MAX_VALUE = 9;
     public static final int MIN_VALUE = 1;
-    private int rule;
-    private GameRule(int rule) { this.rule = rule; }
 }
